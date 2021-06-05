a79b4910e9e6231eaaf3432b249ad0f3104aac4e
Kim Stahl <kim.stahl@gmail.com>
2021-06-05 09:23:13 -0500

    first commit

---
+++ src/main/java/org/launchcode/artist_assistant_app_finalLiftOff/ArtistAssistantAppFinalLiftOffApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.artist_assistant_app_finalLiftOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistAssistantAppFinalLiftOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistAssistantAppFinalLiftOffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/artist_assistant_app_finalLiftOff/ArtistAssistantAppFinalLiftOffApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.artist_assistant_app_finalLiftOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistAssistantAppFinalLiftOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
